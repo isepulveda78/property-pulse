fdb608917f1d5a51da9eb02871208c3bd37f84fa
Israel Sepulveda <sepulveda.israel@gmail.com>
2024-05-08 16:33:25 -0400

    404 and spinner added

--- app/properties/page.jsx
+++ app/properties/page.jsx
@@ -1,8 +1,25 @@
+import properties from '@/data/properties.json'
+import PropertyCard from '@/components/PropertyCard'
 
-const Properties = () => {
+const PropertiesPage = () => {
   return (
-    <div>Properties</div>
+    <section className="px-4 py-6">
+        <div className="container-xl lg:container m-auto px-4 py-6">
+          { properties.length === 0 ? (
+            <p>No properties found.</p>
+            ) : (
+              <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+              { properties.map((property) => (
+                <div>
+                  <PropertyCard key={ property._id } property={ property } />
+                </div>
+              ))}
+            </div>
+            )
+          }
+        </div>
+    </section>
   )
 }
 
-export default Properties
+export default PropertiesPage
