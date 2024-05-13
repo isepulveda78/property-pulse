fb414a5c186e883f3822ef05bfc9fba2200554ca
Israel Sepulveda <sepulveda.israel@gmail.com>
2024-05-13 14:30:12 -0400

    social login added

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,7 @@
 import '@/assets/styles/global.css'
 import Footer from '@/components/Footer'
 import Navbar from '@/components/Navbar'
+import AuthProvider from '@/components/AuthProvider'
 
 export const metadata = {
     title: 'PropertyPulse | Find the Perfect Rental',
@@ -10,13 +11,15 @@ export const metadata = {
 
 const MainLayout = ({ children }) => {
   return (
-    <html lang='en'>
-        <body>
-            <Navbar/>
-              <div>{children}</div>
-            <Footer/>
-        </body>
-    </html>
+    <AuthProvider>
+      <html lang='en'>
+          <body>
+              <Navbar/>
+                <div>{children}</div>
+              <Footer/>
+          </body>
+      </html>
+    </AuthProvider>
   )
 }
 
