bf8262e4e3a9a3e2f85cd8ecc815414d9868cb4e
Israel Sepulveda <sepulveda.israel@gmail.com>
2024-06-03 13:04:26 -0400

    Message System

--- app/layout.jsx
+++ app/layout.jsx
@@ -4,7 +4,7 @@ import Navbar from '@/components/Navbar'
 import AuthProvider from '@/components/AuthProvider'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
-
+import { GlobalProvider } from '@/context/GlobalContext'
 export const metadata = {
     title: 'PropertyPulse | Find the Perfect Rental',
     description: 'Find your dream rental property',
@@ -13,16 +13,18 @@ export const metadata = {
 
 const MainLayout = ({ children }) => {
   return (
-    <AuthProvider>
-      <html lang='en'>
-          <body>
-              <Navbar/>
-                <div>{children}</div>
-              <Footer/>
-              <ToastContainer />
-          </body>
-      </html>
-    </AuthProvider>
+    <GlobalProvider>
+      <AuthProvider>
+        <html lang='en'>
+            <body>
+                <Navbar/>
+                  <div>{children}</div>
+                <Footer/>
+                <ToastContainer />
+            </body>
+        </html>
+      </AuthProvider>
+    </GlobalProvider>
   )
 }
 
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -7,15 +7,15 @@ import logo from '@/assets/images/logo-white.png'
 import profileDefault from '@/assets/images/profile.png'
 import { FaGoogle } from 'react-icons/fa'
 import { signIn, signOut, useSession, getProviders } from 'next-auth/react'
+import UnreadMessageCount from './UnreadMessageCount'
 
 const Navbar = () => {
   const { data: session } = useSession()
-  
   const profileImage = session?.user?.image
 
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false)
   const [isProfileMenuOpen, setIsProfileMenuOpen] = useState(false)
-  const [providers, setProviders] = useState(false)
+  const [providers, setProviders] = useState(null)
 
   const pathname = usePathname()
 
@@ -24,6 +24,7 @@ const Navbar = () => {
       const res = await getProviders()
       setProviders(res)
     }
+
     setAuthProviders()
   }, [])
 
@@ -38,7 +39,7 @@ const Navbar = () => {
               id='mobile-dropdown-button'
               className='relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white'
               aria-controls='mobile-menu'
-              aria-expanded='false'
+              aria-expanded={isMobileMenuOpen}
               onClick={() => setIsMobileMenuOpen((prev) => !prev)}
             >
               <span className='absolute -inset-0.5'></span>
@@ -106,12 +107,17 @@ const Navbar = () => {
           {!session && (
             <div className='hidden md:block md:ml-6'>
               <div className='flex items-center'>
-                { providers && Object.values(providers).map((provider, index) => (
-                <button onClick={() => signIn(provider.id)} key={index} className='flex items-center text-white bg-gray-700 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2'>
-                    <FaGoogle className='text-white mr-2' />
-                    <span>Login or Register</span>
-                  </button>
-                ))}
+                {providers &&
+                  Object.values(providers).map((provider, index) => (
+                    <button
+                      onClick={() => signIn(provider.id)}
+                      key={index}
+                      className='flex items-center text-white bg-gray-700 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2'
+                    >
+                      <FaGoogle className='text-white mr-2' />
+                      <span>Login or Register</span>
+                    </button>
+                  ))}
               </div>
             </div>
           )}
@@ -141,10 +147,7 @@ const Navbar = () => {
                     />
                   </svg>
                 </button>
-                <span className='absolute top-0 right-0 inline-flex items-center justify-center px-2 py-1 text-xs font-bold leading-none text-white transform translate-x-1/2 -translate-y-1/2 bg-red-600 rounded-full'>
-                  2
-                  {/* <!-- Replace with the actual number of notifications --> */}
-                </span>
+                <UnreadMessageCount session={session} />
               </Link>
               {/* <!-- Profile dropdown button --> */}
               <div className='relative ml-3'>
@@ -153,7 +156,7 @@ const Navbar = () => {
                     type='button'
                     className='relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800'
                     id='user-menu-button'
-                    aria-expanded='false'
+                    aria-expanded={isProfileMenuOpen}
                     aria-haspopup='true'
                     onClick={() => setIsProfileMenuOpen((prev) => !prev)}
                   >
@@ -204,14 +207,14 @@ const Navbar = () => {
                       Saved Properties
                     </Link>
                     <button
-                      className='block px-4 py-2 text-sm text-gray-700'
-                      role='menuitem'
-                      tabIndex='-1'
-                      id='user-menu-item-2'
                       onClick={() => {
                         setIsProfileMenuOpen(false)
                         signOut()
                       }}
+                      className='block px-4 py-2 text-sm text-gray-700'
+                      role='menuitem'
+                      tabIndex='-1'
+                      id='user-menu-item-2'
                     >
                       Sign Out
                     </button>
@@ -254,14 +257,17 @@ const Navbar = () => {
               </Link>
             )}
 
-            {!session && 
-                 providers && 
-                 Object.values(providers).map((provider, index) => (
-                  <button onClick={() => signIn(provider.id)} key={index} className='flex items-center text-white bg-gray-700 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2 my-4'>
-                          <span>Login or Register</span>
-                  </button>
-                ))
-            }
+            {!session &&
+              providers &&
+              Object.values(providers).map((provider, index) => (
+                <button
+                  onClick={() => signIn(provider.id)}
+                  key={index}
+                  className='flex items-center text-white bg-gray-700 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2'
+                >
+                  <span>Login or Register</span>
+                </button>
+              ))}
           </div>
         </div>
       )}
